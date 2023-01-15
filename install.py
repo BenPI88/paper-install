fff03254a81051f5572f92c2ae4686cfb04ce777
BenPI88 <97198967+BenPI88@users.noreply.github.com>
2023-01-15 16:09:15 -0600

    Create install.py

---
+++ install.py
@@ -0,0 +1,8 @@
+#d8888b.  .d8b.  d8888b. d88888b d8888b.      d888888b d8b   db .d8888. d888888b  .d8b.  db      db      d88888b d8888b. 
+#88  `8D d8' `8b 88  `8D 88'     88  `8D        `88'   888o  88 88'  YP `~~88~~' d8' `8b 88      88      88'     88  `8D 
+#88oodD' 88ooo88 88oodD' 88ooooo 88oobY'         88    88V8o 88 `8bo.      88    88ooo88 88      88      88ooooo 88oobY' 
+#88~~~   88~~~88 88~~~   88~~~~~ 88`8b           88    88 V8o88   `Y8b.    88    88~~~88 88      88      88~~~~~ 88`8b   
+#88      88   88 88      88.     88 `88.        .88.   88  V888 db   8D    88    88   88 88booo. 88booo. 88.     88 `88. 
+#88      YP   YP 88      Y88888P 88   YD      Y888888P VP   V8P `8888Y'    YP    YP   YP Y88888P Y88888P Y88888P 88   YD 
+#
+#Made By https://github.com/BenPI88
